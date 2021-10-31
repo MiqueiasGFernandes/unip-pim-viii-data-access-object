4af8cdeee3955527a14a7b419a9b7e9a6ea9be17
Miqueias Fernandes <miqueiasgomesfernandes@gmail.com>
2021-10-31 03:10:22 +0000

    feat: add DAO methods

---
+++ unip-pim-viii-data-access-object/Models/PessoaDAO.cs
@@ -0,0 +1,66 @@
+﻿namespace unip_pim_viii_data_access_object.Models.DAO
+{
+    static public class PessoaDAO
+    {
+        private static PIMContext? connection;
+
+        public static PIMContext setConnection(PIMContext context)
+        {
+            if(connection == null)
+            {
+                connection = context;
+            };
+            return connection;
+        }
+
+        public static bool exclua(Pessoa p) {
+            try
+            {
+                connection.Remove(p);
+                connection.SaveChanges();
+
+                return true;
+            } catch (Exception e)
+            {
+                return false;
+            }
+        }
+        public static bool insere(Pessoa p) {
+            try{
+                connection.Add(p);
+                connection.SaveChanges();
+
+                return true;
+            } catch (Exception e)
+            {
+                return false;
+            }
+        }
+        public static bool consulta(Pessoa p) {
+            try
+            {
+                connection.Find(p);
+                connection.SaveChanges();
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+        public static bool altere(Pessoa p) {
+            try
+            {
+                connection.Update(p);
+                connection.SaveChanges();
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+    }
+}
